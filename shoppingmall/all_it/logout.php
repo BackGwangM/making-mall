6504942021507d75ea57ba57117d0971f3ee45f2
bgm <back5373122@gmail.com>
2017-08-22 18:20:13 +0900

    초기설정, 커밋 실수 고침

--- shoppingmall/all_it/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    session_destroy();
-    echo '<script>alert("성공적으로 로그아웃 되었습니다!");
-        location.href="http://localhost/homework_3/homework_4/index.php";
-    </script>';
-    
-?>
